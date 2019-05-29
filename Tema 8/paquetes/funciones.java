8b2831a59a649a6a11d6bd5285a86073919b45a2
Cristina Castillo Obregón <cristinacastilloob@gmail.com>
2019-05-29 10:09:41 +0200

    actualización

--- Tema 8/paquetes/funciones.java
+++ Tema 8/paquetes/funciones.java
@@ -386,6 +386,127 @@ public class funciones {
     return x;
     
   }
+
+/rotar a la derecha x columnas
+    public static int[][] rotarDerecha(int[][] x, int rotacion) {
+        int[][] resultado = new int[x.length][x[0].length];
+        rotacion = rotacion % x[0].length;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                resultado[i][(j + rotacion) % x[i].length] = x[i][j];
+            }
+        }
+
+        return resultado;
+    }
+
+    //rotar a la dcha x columnas en dos funciones
+    public static int[][] rotarDerechaBis(int[][] x, int rotacion) {
+        int[][] resultado;
+        resultado = duplicar(x);
+
+        for (int i = 0; i < rotacion; i++) {
+            resultado = rotarDerecha1(resultado);
+        }
+
+        return resultado;
+    }
+
+    
+    
+    public static int[][] rotarDerecha1(int x[][]) {
+        int[][] resultado;
+        int aux;
+        resultado = duplicar(x);
+
+        for (int i = 0; i < resultado.length; i++) {
+            aux = resultado[i][resultado[i].length - 1];
+            for (int j = resultado[i].length - 1; j > 0; j--) {
+                resultado[i][j] = resultado[i][j - 1];
+            }
+            resultado[i][0] = aux;
+        }
+
+        return resultado;
+    }
+
+    
+    //rotar abajo x filas
+    public static int[][] rotarAbajo(int[][] x, int rotacion) {
+        int[][] resultado = new int[x.length][x[0].length];
+        rotacion = rotacion % x.length;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                //resultado[i][(j+rotacion)%x[i].length] = x[i][j];
+                resultado[(i + rotacion) % x.length][j] = x[i][j];
+            }
+        }
+
+        return resultado;
+    }
+
+    
+    //rotar abajo x filas en dos funciones
+    public static int[][] rotarAbajoBis(int[][] x, int rotacion) {
+        int[][] resultado = duplicar(x);
+        rotacion = rotacion % x.length;
+        for (int i = 0; i < rotacion; i++) {
+            resultado = rotarAbajo1(resultado);
+        }
+
+        return resultado;
+    }
+
+    public static int[][] rotarAbajo1(int[][] x) {
+        int[][] resultado = duplicar(x);
+        int aux;
+        for (int j = 0; j < resultado[0].length; j++) {
+            aux = resultado[resultado.length - 1][j];
+            for (int i = resultado.length - 1; i > 0; i--) {
+                resultado[i][j] = x[i - 1][j];
+            }
+            resultado[0][j] = aux;
+        }
+
+        return resultado;
+    }
+
+    
+    //duplicar una matriz
+    public static int[][] duplicar(int[][] x) {
+        int[][] resultado = new int[x.length][x[0].length];
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                resultado[i][j] = x[i][j];
+            }
+        }
+        return resultado;
+    }
+/**/
+
+//OBTENER MATRIZ TRASPUESTA, CAMBIAR FILAS POR COLUMNAS
+public static int[][] traspuesta(int[][] matriz) {
+
+        int[][] resultado;
+        int filas, columnas;
+        filas = matriz.length;
+        columnas = matriz[0].length;
+
+        resultado = new int[columnas][filas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+
+                resultado[j][i] = matriz[i][j];
+
+            }
+
+        }
+
+        return resultado;
+    }
+
+
 //29
 
   public static int[][] generaArrayBiInt(int f, int c, int max, int min) {
